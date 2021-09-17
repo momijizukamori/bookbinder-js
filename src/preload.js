57e4e080eb9b847fab9c14458187d15ad17275dd
momijizukamori <momijizukamori@gmail.com>
2021-09-17 14:26:39 -0400

    Add setting restore

--- src/preload.js
+++ src/preload.js
@@ -3,9 +3,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const paperlist = document.getElementById('paper_size');
   const generate = document.getElementById("generate");
+  const storageKey = "bookbinderSettings";
   const settings = window.localStorage;
   let bookbinderForm = document.getElementById('bookbinder');
-  loadForm();
+
 
 
   Object.keys(pagesizes).forEach(key => {
@@ -17,6 +18,8 @@ window.addEventListener('DOMContentLoaded', () => {
     opt.innerText = key;
     paperlist.appendChild(opt);
   });
+
+  loadForm();
 
     let book = new Book();
     let inputs = document.querySelectorAll('input, select');
@@ -71,13 +74,14 @@ window.addEventListener('DOMContentLoaded', () => {
     function saveForm(formData) {
       let result = (({ duplex, duplexrotate, format, sigsize, lockratio }) => ({ duplex, duplexrotate, format, sigsize, lockratio }))(book);
       result.papersize = formData.get('paper_size');
-      settings.setItem('bookbinderSettings', JSON.stringify(result));
+      result.booksize = formData.get('book_size');
+      settings.setItem(storageKey, JSON.stringify(result));
     }
 
     function loadForm(){
-      const bookSettings = JSON.parse(settings.getItem('bookbinderSetings'));
-      console.log(settings.getItem('bookbinderSetings'));
+      const bookSettings = JSON.parse(settings.getItem(storageKey));
       if (bookSettings) {
+        try {
         if (bookSettings.duplex) {
           document.querySelector('#printer_type > option[value="duplex"]').setAttribute('selected', "");
           document.querySelector('#printer_type > option[value="single"]').removeAttribute('selected');
@@ -85,15 +89,30 @@ window.addEventListener('DOMContentLoaded', () => {
           document.querySelector('#printer_type > option[value="single"]').setAttribute('selected', "");
           document.querySelector('#printer_type > option[value="duplex"]').removeAttribute('selected');
         }
+
+        if (bookSettings.lockratio) {
+          document.querySelector('option[value="lockratio"]').setAttribute('selected', "");
+          document.querySelector('option[value="stretch"]').removeAttribute('selected');
+        } else {
+          document.querySelector('option[value="stretch"]').setAttribute('selected', "");
+          document.querySelector('option[value="lockratio"]').removeAttribute('selected');
+        }
+
         if (bookSettings.duplexrotate) {
           document.querySelector('input[name="rotate_page"').setAttribute('checked', "");
         } else {
           document.querySelector('input[name="rotate_page"').removeAttribute('checked');
         }
+
+        document.querySelector('option[value="A4"]').removeAttribute('selected');
+       document.querySelector('option[value="'+ bookSettings.papersize + '"]').setAttribute('selected', "");
   
         document.getElementById(bookSettings.format).setAttribute('checked', "");
+        document.getElementById(bookSettings.booksize).setAttribute('checked', "");
         document.querySelector('input[name="sig_length').setAttribute('value', bookSettings.sigsize);
+      } catch(error) {
+        console.log(error);
       }
-
+    }
     }
   });
