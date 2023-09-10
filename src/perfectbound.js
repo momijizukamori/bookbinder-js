aaffd1a7050e1f52d34501e74e13076c680da785
krishnakrish24 <81485889+krishnakrish24@users.noreply.github.com>
2023-09-10 10:30:17 +0530

    Add license to all files

--- src/perfectbound.js
+++ src/perfectbound.js
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+
 export class PerfectBound {
 
 	//  Duplex printer pages are arranged in a 4-1-2-3 pattern.
