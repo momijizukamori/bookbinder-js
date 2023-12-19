af9f621407e420996a629365846826a061aa925b
cass <kazumi_mori+github@pm.me>
2023-12-19 13:41:47 -0600

    Add license preamble to new files

--- src/utils/uri.js
+++ src/utils/uri.js
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+
 /**
  * Gets parameters from a URL.
  * @param { string } url The URL to get the params from
