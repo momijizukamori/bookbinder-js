0ec7df32cc8d0b03aa09a4dee092b09183a803d4
momijizukamori <momijizukamori+bugzilla@gmail.com>
2024-02-09 13:27:41 -0500

    Add Prettier and ESLint (#88)
    * Add Prettier and ESLint
    
    ---------
    
    Co-authored-by: Cocoa <momijizukamori@gmail.com>

--- src/wacky_imposition.test.js
+++ src/wacky_imposition.test.js
@@ -1,25 +1,25 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import { expect, describe, it } from 'vitest';
 
 import { WackyImposition } from './wacky_imposition';
 
 describe('WackyImposition model', () => {
-    it('returns a new instance of a WackyImposition', () => {
-        const testPages = [];
-        const testDuplex = true;
-        const testFormat = 'standardsig';
-        const expected = {
-            duplex: true,
-            sigconfig: [],
-            pagelistdetails: [[]],
-            sheets: 0,
-        };
-        const actual = new WackyImposition(testPages, testDuplex, testFormat);
-        console.log(actual);
-        expect(actual).toEqual(expected);
-    });
-	// TODO add tests with actual pages
+  it('returns a new instance of a WackyImposition', () => {
+    const testPages = [];
+    const testDuplex = true;
+    const testFormat = 'standardsig';
+    const expected = {
+      duplex: true,
+      sigconfig: [],
+      pagelistdetails: [[]],
+      sheets: 0,
+    };
+    const actual = new WackyImposition(testPages, testDuplex, testFormat);
+    console.log(actual);
+    expect(actual).toEqual(expected);
+  });
+  // TODO add tests with actual pages
 });
