dc4a99b782ad5285d5543943a3fc8779cf373df6
Cocoa <momijizukamori@gmail.com>
2024-03-05 18:47:34 -0500

    Split HTML to make it easier to edit.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import version from 'vite-plugin-package-version';
+import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
   base: 'https://momijizukamori.github.io/bookbinder-js/',
   test: {
     environment: 'jsdom',
   },
-  plugins: [version()],
+  plugins: [version(), injectHTML()],
 });
