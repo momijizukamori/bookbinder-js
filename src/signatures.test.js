36864245c39cf8c43b2f9e23ea0e8fd8c8f41c94
Patrick Boatner <patrickboatner@gmail.com>
2025-11-09 04:14:41 -0500

    failing test case for 32 page test

--- src/signatures.test.js
+++ src/signatures.test.js
@@ -100,6 +100,37 @@ describe('Signatures model', () => {
         ]);
       });
     });
+
+    describe('a 32 page PDF, with signature length = 2 and flyleafs = 0', () => {
+      const pages = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+        25, 26, 27, 28, 29, 30, 31,
+      ];
+      const sigsize = 4;
+
+      it('computes pagelistdetails correctly', () => {
+        const signatures = new Signatures(pages, sigsize, per_sheet, duplexrotate);
+        signatures.createsigconfig();
+
+        const infos = extractPageInfos(signatures.pagelistdetails);
+
+        // Current behavior. Test passes, but it's wrong
+        // expect(infos).toEqual([
+        //   [
+        //     [19, 12, 28, 3, 25, 6, 22, 9, 27, 4, 20, 11, 17, 14, 30, 1],
+        //     [29, 2, 18, 13, 23, 8, 24, 7, 21, 10, 26, 5, 31, 0, 16, 15],
+        //   ],
+        // ]);
+
+        // Desired behavior. Test is failing right now.
+        expect(infos).toEqual([
+          [
+            [23, 8, 24, 7, 21, 10, 26, 5, 19, 12, 28, 3, 17, 14, 30, 1],
+            [25, 6, 22, 9, 27, 4, 20, 11, 29, 2, 18, 13, 31, 0, 16, 15]
+          ],
+        ]);
+      });
+    });
   });
 });
 
