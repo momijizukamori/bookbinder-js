77a6eb27d7744f447008d5372dbf73d3a25203ae
Patrick Boatner <patrickboatner@gmail.com>
2025-12-06 22:24:58 -0500

    add test case for existing behavior

--- src/wacky_imposition.test.js
+++ src/wacky_imposition.test.js
@@ -22,4 +22,52 @@ describe('WackyImposition model', () => {
     expect(actual).toEqual(expected);
   });
   // TODO add tests with actual pages
+
+  describe('build_1_3rd_sheetList', () => {
+    const testPages = [];
+    const duplex = true;
+    const testFormat = '1_3rd';
+
+    describe('a simple 12 page input file', () => {
+
+      it('generates 2 sheets with the correct pages', async () => {
+        const expected = [
+          [
+            [
+              { "num": 7, "isBlank": false, "vFlip": false },
+              { "num": 4, "isBlank": false, "vFlip": false }
+            ],
+            [
+              { "num": 8, "isBlank": false, "vFlip": true },
+              { "num": 3, "isBlank": false, "vFlip": true }
+            ],
+            [
+              { "num": 11, "isBlank": false, "vFlip": false },
+              { "num": 0, "isBlank": false, "vFlip": false }
+            ]
+          ],
+          [
+            [
+              { "num": 5, "isBlank": false, "vFlip": false },
+              { "num": 6, "isBlank": false, "vFlip": false }
+            ],
+            [
+              { "num": 2, "isBlank": false, "vFlip": true },
+              { "num": 9, "isBlank": false, "vFlip": true }
+            ],
+            [
+              { "num": 1, "isBlank": false, "vFlip": false },
+              { "num": 10, "isBlank": false, "vFlip": false }
+            ]
+          ]
+        ];
+
+        const actual = new WackyImposition(testPages, duplex, testFormat).build_1_3rd_sheetList(12);
+
+        expect(actual).toEqual(expected);
+      });
+    });
+
+
+  });
 });
